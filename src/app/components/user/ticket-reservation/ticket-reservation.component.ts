c4f1bf477d82f2c759b72eba09443a2f37a5d43d
Janko <ljubicjanko1@gmail.com>
2020-02-03 21:12:34 +0100

    forgot to create branch

--- src/app/components/user/ticket-reservation/ticket-reservation.component.ts
+++ src/app/components/user/ticket-reservation/ticket-reservation.component.ts
@@ -15,11 +15,12 @@ import { AuthService, AuthQuery } from 'src/app/shared/store';
 import { ParterDto } from 'src/app/shared/model/ParterDto';
 import { SeatDto } from 'src/app/shared/model/SeatDto';
 import { PayPalService } from 'src/app/shared/services/paypal/paypal.service';
+import { EventDayState } from 'src/app/shared/model/EventDayState';
 import { SnackbarComponent } from '../../common/snackbar/snackbar.component';
 import { ConfirmDialogComponent } from '../../common/confirm-dialog/confirm-dialog.component';
 
 /**
- * Main component for ticket purchase. Contains location layout, 
+ * Main component for ticket purchase. Contains location layout,
  * picked seats review and buttons for reserving an buying tickets.
  */
 @Component({
@@ -48,6 +49,9 @@ export class TicketReservationComponent implements OnInit {
   user: any;
 
   loadingNeeded: boolean = false;
+
+  reservable: boolean = false;
+  buyable: boolean = false;
 
 
   constructor(
@@ -83,9 +87,29 @@ export class TicketReservationComponent implements OnInit {
     this.fetchData();
   }
 
+  determineState(): void {
+    if (typeof (this.eventDay) != "undefined") {
+
+      switch (this.eventDay.eventDayState) {
+        case EventDayState[EventDayState.RESERVABLE_AND_BUYABLE]:
+          this.reservable = true;
+          this.buyable = true;
+          break;
+        case EventDayState[EventDayState.ONLY_BUYABLE]:
+          this.reservable = false;
+          this.buyable = true;
+          break;
+        default:
+          this.reservable = false;
+          this.buyable = false;
+          break;
+      }
+    }
+  }
+
   /**
    * Deletes seat from table of picked seats
-   * @param seat 
+   * @param seat
    */
   removeSeat(seat: any): void {
     const index: number = this.seats.indexOf(seat);
@@ -100,7 +124,7 @@ export class TicketReservationComponent implements OnInit {
 
   /**
    * Deletes tickets for certain parter from parter tickets table
-   * @param parter 
+   * @param parter
    */
   removeParter(parter: any): void {
     let index: number;
@@ -133,7 +157,7 @@ export class TicketReservationComponent implements OnInit {
 
   /**
    * Opens dialog for entering number of tickets in case of parter, or for picking seat in case of grandstand
-   * @param sector 
+   * @param sector
    */
   openPopup(sector: any): void {
     if (sector.type === "PARTER") {
@@ -177,7 +201,7 @@ export class TicketReservationComponent implements OnInit {
 
   /**
    * Makes changes in tickets list based on currently picked seats
-   * @param res 
+   * @param res
    */
   updateTickest(res: any): any {
     res.forEach(ticket => {
@@ -187,6 +211,7 @@ export class TicketReservationComponent implements OnInit {
         }
       });
     });
+    this.determineState();
     return res;
   }
 
@@ -229,6 +254,7 @@ export class TicketReservationComponent implements OnInit {
 
     let clonedSec = this.sectors.slice();
     this.sectors = clonedSec;
+    this.determineState();
   }
 
 
@@ -245,7 +271,7 @@ export class TicketReservationComponent implements OnInit {
 
   purchase(buy: boolean): void {
     this.loadingNeeded = true;
-    
+
     let pickedParters: Array<ParterDto> = new Array<ParterDto>();
     let pickedSeats: Array<SeatDto> = new Array<SeatDto>();
 
@@ -272,20 +298,20 @@ export class TicketReservationComponent implements OnInit {
 
     this.eventService.reserve(reservationDto).subscribe((res) => {
       console.log(res);
-      if(buy){
+      if (buy) {
         window.location.replace(res.redirect_url);
-      }else{
+      } else {
         this.snackbar.openFromComponent(SnackbarComponent, {
-          data: "Reservation was successful!", 
+          data: "Reservation was successful!",
           panelClass: ['snackbar-success'],
         });
-  
+
         this.router.navigate(['/']);
       }
     },
       error => {
         this.snackbar.openFromComponent(SnackbarComponent, {
-          data: "Reservation failed!", 
+          data: "Reservation failed!",
           panelClass: ['snackbar-error'],
         });
         this.router.navigate(['/']);
