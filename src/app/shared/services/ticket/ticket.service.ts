11d4b9ee1a3d493cb1bbc0555a4c1502ee375355
Janko <ljubicjanko1@gmail.com>
2020-02-05 14:58:20 +0100

    Done

--- src/app/shared/services/ticket/ticket.service.ts
+++ src/app/shared/services/ticket/ticket.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { User, AuthStore,AuthQuery } from '../../store';
+import { User, AuthStore, AuthQuery } from '../../store';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tap, catchError, debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { Observable, of, throwError as observableThrowError} from 'rxjs';
-import { HttpErrorResponse} from '@angular/common/http';
+import { Observable, of, throwError as observableThrowError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 const ENDPOINTS = {
   ALL_TICKETS: '/ticket/',
-  CANCEL_RESERVATION: "/ticket/cancel/"
+  CANCEL_RESERVATION: "/ticket/cancel/",
+  CONFIRM_RESERVATION: "/ticket/buy/"
 };
 
 @Injectable({
@@ -16,13 +17,18 @@ const ENDPOINTS = {
 
 export class TicketService {
   constructor(private http: HttpClient) {
-     
-    }
-   
+
+  }
+
+
+  confirmReservation(ticketId: string): Observable<any> {
+    return this.http.put(ENDPOINTS.CONFIRM_RESERVATION + ticketId, {});
+  }
+
   cancelReservation(ticketId): Observable<any> {
     return this.http.put(ENDPOINTS.CANCEL_RESERVATION + ticketId, {});
   }
-  
+
   getAllTickets(sectorId: number, eventDayId: number): Observable<any> {
     return this.http.get(ENDPOINTS.ALL_TICKETS + sectorId + '/' + eventDayId);
   }
