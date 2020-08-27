224f14134f545bc0ebf73df0dd97c9f1b00d1f97
Janko <ljubicjanko1@gmail.com>
2020-08-27 20:40:32 +0200

    made some progress on user aspect

---
+++ cypress/integration/user/edit-profile.spec.js
@@ -0,0 +1,26 @@
+const { createYield } = require("typescript")
+
+describe('User editing profile information', function() {
+    it.only('Edit user information', function() {
+        cy.SignInUser()
+
+
+        cy.get('#profile-button').should('be.visible').click()
+        cy.url().should('include', '/profile')
+
+        cy.get('#editProfileButton').should('be.visible').click()
+        cy.url().should('include', '/edit-profile')
+
+        cy.get('#firstNameInput').clear().type('Blagoje')
+        cy.get('#lastNameInput').clear().type('Jovović')
+        cy.get('#emailInput').clear().type('jovovic@gmail.com')
+    
+        cy.get('#updateButton').should('be.visible').click()
+
+        cy.get('.mat-snack-bar-container', { timeout: 5000 }).contains('You have successfully updated your profile').should('be.visible')
+        cy.get('#firstName').contains('Blagoje').should('be.visible')
+        cy.get('#lastName').contains('Jovović').should('be.visible')
+        cy.get('#email').contains('jovovic@gmail.com').should('be.visible')
+
+    })
+})
